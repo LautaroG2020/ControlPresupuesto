4afafa6d63f149c7bb35242d8c44b3befad57d0c
Lautaro Galván <65605801+LautaroG2020@users.noreply.github.com>
2020-10-21 10:51:01 -0300

    Add files via upload

---
+++ build/precache-manifest.bd523a7d901149123fb12bebcd6b84d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dab929f11e436d20e3df844ca82e9df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a09547744ea8a148b52",
+    "url": "/static/css/main.90d52d0a.chunk.css"
+  },
+  {
+    "revision": "35ff1e67a448da6e77b4",
+    "url": "/static/js/2.ce226546.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce226546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a09547744ea8a148b52",
+    "url": "/static/js/main.78410ce9.chunk.js"
+  },
+  {
+    "revision": "29630ad1ca7e967d236a",
+    "url": "/static/js/runtime-main.ea644902.js"
+  }
+]);
